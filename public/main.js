6e87c0dfcd945f39668c8bea1dbf07bdc9e2c981
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-11-23 15:18:56 +0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,11 @@
+// Membuat bisa responsive tombolnya
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () =>{
+    if (menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }else{
+        menu.classList.add('hidden');
+    }
+});
